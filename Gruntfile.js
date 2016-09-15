d2ceac53b86d28f3c4622f58201f4415b378874b
tuckermullen <mullentucker@gmail.com>
2016-09-15 14:18:18 -0700

    initial commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,3 +79,24 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-hapi');
 
 };
+
+module.exports = function(grunt) {
+    
+    grunt.registerTask('default', 'Testing out Grunt logging and task callbacks', ['watch']);
+    
+    grunt.registerTask('fun', 'This task is for fun only', function() {
+        grunt.log.writeln('This the *fun* Grunt task');
+    });
+    
+    grunt.registerTask('serious', 'This task is for serious stuff only', function() {
+        grunt.log.writeln('Wipe that smirk off your facel this is serious.');
+    });
+    
+    grunt.initConfig({
+        watch: {
+            files: ['Gruntfile.js']
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
