9b09c5a54c6dee17301be89ac973b3fdc8cc5e29
tuckermullen <mullentucker@gmail.com>
2016-09-22 10:03:53 -0700

    Checkpoint 5 complete

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -9,15 +9,17 @@
         $stateProvider
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
         })
-        .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
         })
-        .state('collection', {
-            url: '/collection',
-            templateUrl: '/templates/collection.html'
+            .state('collection', {
+                url: '/collection',
+                controller: 'CollectionCtrl as collection',
+                templateUrl: '/templates/collection.html'
         });
     }
     
