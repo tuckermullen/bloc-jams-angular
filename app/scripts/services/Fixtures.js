1c83820fc105ab0869ec5741df80138b1c9e8f47
tuckermullen <mullentucker@gmail.com>
2016-09-27 15:36:39 -0700

    Assignment 6 complete

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,16 @@
         return albumPicasso;
     };
         
+    Fixtures.getCollection = function(numberOfAlbums) {
+        var albums = [];
+        
+        for (var i = 0; i < numberOfAlbums; i++)
+            {
+                albums.push(albumPicasso);
+            }
+        return albums;
+    };
+        
         return Fixtures;
     }
     
