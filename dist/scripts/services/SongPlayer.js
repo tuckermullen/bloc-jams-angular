990f87a42d58e6b7ffe4a6da182bd1662e652942
tuckermullen <mullentucker@gmail.com>
2016-09-27 16:40:49 -0700

    Checkpoint 7 complete

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -3,20 +3,36 @@
         var SongPlayer = {};
         
         var currentSong = null;
+        
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
+        
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
+        
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+            
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+            
+            currentSong = song;
+        };
         
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
-                if (currentBuzzObject) {
-                    currentBuzzObject.stop();
-                    currentSong.playing = null;
-                }
-                var currentBuzzObject = new buzz.sound(song.audioUrl, {
-                    formats: ['mp3'],
-                    preload: true
-                });
-                
-                currentSong = song;
+                setSong(song);
                 currentBuzzObject.play();
                 song.playing = true;
             } else if (currentSong === song) {
@@ -24,6 +40,11 @@
                     currentBuzzObject.play();
                 }
             }
+        };
+        
+        SongPlayer.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
         };
         
         return SongPlayer;
